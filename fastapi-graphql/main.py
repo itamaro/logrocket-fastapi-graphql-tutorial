5213a60616971f9c8d60d517a87f54ca9d5c0271
Itamar Ostricher <itamarost@gmail.com>
2021-04-22 15:00:20 -0700

    Implement createCourse GraphQL mutation

--- fastapi-graphql/main.py
+++ fastapi-graphql/main.py
@@ -1,7 +1,7 @@
 import json
 
 from fastapi import FastAPI
-from graphene import Field, List, ObjectType, Schema, String
+from graphene import Field, List, Mutation, ObjectType, Schema, String
 from graphql.execution.executors.asyncio import AsyncioExecutor
 from starlette.graphql import GraphQLApp
 import uvicorn
@@ -21,9 +21,33 @@ class Query(ObjectType):
         return [course for course in course_list if course["id"] == id]
 
 
+class CreateCourse(Mutation):
+    course = Field(CourseType)
+
+    class Arguments:
+        id = String(required=True)
+        title = String(required=True)
+        instructor = String(required=True)
+
+    async def mutate(self, info, id, title, instructor):
+        with open("./courses.json", "r+") as courses:
+            course_list = json.load(courses)
+            course_list.append({"id": id, "title": title, "instructor": instructor})
+            courses.seek(0)
+            json.dump(course_list, courses, indent=2)
+        return CreateCourse(course=course_list[-1])
+
+
+class Mutation(ObjectType):
+    create_course = CreateCourse.Field()
+
+
 app = FastAPI()
 app.add_route(
-    "/", GraphQLApp(schema=Schema(query=Query), executor_class=AsyncioExecutor)
+    "/",
+    GraphQLApp(
+        schema=Schema(query=Query, mutation=Mutation), executor_class=AsyncioExecutor
+    ),
 )
 
 
